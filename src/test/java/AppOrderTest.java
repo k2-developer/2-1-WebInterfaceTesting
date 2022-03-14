4dff5a8c77f72ab9e82f769ae853d092c7a2e1da
Smetskii Ivan <smetskii.ivan@gmail.com>
2022-03-14 17:25:40 +0300

    Some changes

---
+++ src/test/java/AppOrderTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\JAVA\\2.1.-testWebInterface\\driver\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestForm(){
+        driver.get("http://localhost:9999");
+        WebElement textElements = driver.findElement(By.className("input__control"));
+        textElements.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivan");
+    }
+    @Test
+    void test(){
+        WebElement element = driver.findElement(By.id("root"));
+        String text = element.getText();
+    }
+
+}
